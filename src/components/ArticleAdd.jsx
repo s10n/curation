153810517c9c5746f26395ad54443d5bb042228c
심철환 <a@akaiv.com>
2017-08-15 16:42:27 +0900

    Show Article for preview on ArticleAdd

--- src/components/ArticleAdd.jsx
+++ src/components/ArticleAdd.jsx
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types'
 import axios from 'axios'
 import api from '../config/api'
 import Input from './Input'
+import Article from './Article'
 
 const propTypes = {
   onAdd: PropTypes.func.isRequired
@@ -20,16 +21,21 @@ class ArticleAdd extends Component {
 
     axios
       .get(api.url + url, { headers })
-      .then(({ data }) => this.setState({ data }))
+      .then(({ data }) => this.setState({ data, error: {} }))
       .catch(error => this.setState({ error }))
   }
 
   render() {
+    const { data, error } = this.state
+
     return (
-      <Input
-        onChange={_.debounce(url => this.requestParse(url), 250)}
-        onEnter={() => this.props.onAdd(this.state.data)}
-      />
+      <section>
+        <Input
+          onChange={_.debounce(url => url && this.requestParse(url), 250)}
+          onEnter={() => data.url && this.props.onAdd(data)}
+        />
+        {error.message || (data.url && <Article {...data} />)}
+      </section>
     )
   }
 }
