6657b7a5e7e08863420e4ec0568025cf28e9f0c6
심철환 <a@akaiv.com>
2017-08-28 23:51:26 +0900

    Provide type to Input component

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 const propTypes = {
+  type: PropTypes.string,
   value: PropTypes.string,
   placeholder: PropTypes.string,
   onChange: PropTypes.func.isRequired,
@@ -9,12 +10,14 @@ const propTypes = {
 }
 
 const defaultProps = {
+  type: 'text',
   value: '',
   placeholder: ''
 }
 
-const Input = ({ value, placeholder, onChange, onEnter }) =>
+const Input = ({ type, value, placeholder, onChange, onEnter }) =>
   <input
+    type={type}
     value={value}
     placeholder={placeholder}
     onChange={event => onChange(event.target.value)}
